276715a4a7e54b076509ae31b80e7a73f9f89e35
Enoc Emanuel <eehcx.contacto@gmail.com>
2023-03-26 22:49:35 -0600

    colisionews y cinemachine

--- Assets/Scripts/movimiento.cs
+++ Assets/Scripts/movimiento.cs
@@ -3,23 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movimiento : MonoBehaviour
-{   
+{
     public float speed = 4f;
 
     Animator anim;
     Rigidbody2D rb2d;
     Vector2 mov;
-
-
-    CircleCollider2D attackCollider;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
-
-        attackCollider = transform.GetChild(0).GetComponent<CircleCollider2D>();
-        attackCollider.enabled=false;
     }
 
     // Update is called once per frame
@@ -30,49 +24,34 @@ public class movimiento : MonoBehaviour
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")
         );
-/*
-        Vector3 mov = new Vector3(
-            Input.GetAxisRaw("Horizontal"),
-            Input.GetAxisRaw("Vertical"),
-            0
-        );
-        transform.position = Vector3.MoveTowards(
+        /*
+                Vector3 mov = new Vector3(
+                    Input.GetAxisRaw("Horizontal"),
+                    Input.GetAxisRaw("Vertical"),
+                    0
+                );
+                transform.position = Vector3.MoveTowards(
 
-            transform.position,
-            transform.position+ mov,
-            speed * Time.deltaTime
-        );
-*/
+                    transform.position,
+                    transform.position+ mov,
+                    speed * Time.deltaTime
+                );
+        */
 
-        if(mov != Vector2.zero){
-        anim.SetFloat("movX",mov.x);
-        anim.SetFloat("movY",mov.y);
-        anim.SetBool("walking",true);
-        }else{
-            anim.SetBool("walking",false);
+        if (mov != Vector2.zero)
+        {
+            anim.SetFloat("movX", mov.x);
+            anim.SetFloat("movY", mov.y);
+            anim.SetBool("walking", true);
+        }
+        else
+        {
+            anim.SetBool("walking", false);
         }
         //prueba
-
-
-        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-        bool attacking = stateInfo.IsName("player_attack");
-        if (Input.GetKeyDown("space") && !attacking ){
-                anim.SetTrigger("attacking");
-        }
-
-
-        if (mov != Vector2.zero) attackCollider.offset = new Vector2(mov.x/2,mov.y/2);
-    
-
-    if (attacking){
-        float playbackTime = stateInfo.normalizedTime;
-        if (playbackTime > 0.33 && playbackTime < 0.66) attackCollider.enabled=true;
-        else attackCollider.enabled= false;
-
-
     }
-    }
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
 
         rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
     }
